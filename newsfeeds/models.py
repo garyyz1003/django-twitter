009d575833d4eb2b4a4f75739a582d4b1e2c42f7
garyyz1003 <garyyz1003@gmail.com>
2021-05-09 23:23:09 -0400

    added a comment

--- newsfeeds/models.py
+++ newsfeeds/models.py
@@ -5,6 +5,7 @@ from tweets.models import Tweet
 
 class NewsFeed(models.Model):
     # 注意此处的user指的不是谁发的贴 而是谁能看到该帖子（tweet）
+    # on_delete=models.SET_NULL 否则默认是cascade会产生级联删除
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     tweet = models.ForeignKey(Tweet, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
